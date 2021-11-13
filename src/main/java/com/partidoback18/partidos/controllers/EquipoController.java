8f9668e49ddb078f7472010c406e3fb5b30e730e
Combatt17 <carloscombatt17@gmail.com>
2021-11-13 18:34:24 -0500

    Añadiendo metodo de error EquipoController

--- src/main/java/com/partidoback18/partidos/controllers/EquipoController.java
+++ src/main/java/com/partidoback18/partidos/controllers/EquipoController.java
@@ -6,11 +6,14 @@ import java.util.Map;
 
 import javax.validation.Valid;
 
+import com.partidoback18.partidos.exceptions.CustomException;
 import com.partidoback18.partidos.models.EquipoModel;
 import com.partidoback18.partidos.services.EquipoService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,7 +28,11 @@ public class EquipoController {
     EquipoService equipoService;
 
     @PostMapping("/equipos") //Define la petición HTTP con la que ejecutará el método (POST) y El path (/api/partidos)
-    public ResponseEntity<Map<String, String>> guardar(@Valid @RequestBody EquipoModel equipo){
+    public ResponseEntity<Map<String, String>> guardar(@Valid @RequestBody EquipoModel equipo, Errors error){
+       //Verificamos si existe un error
+        if(error.hasErrors()){
+            throwError(error);
+        }
         //Recibimos los datos por el body de la petición
         this.equipoService.guardarEquipo(equipo); //Invocamos el metodo creado en el servicio
         Map<String, String> respuesta=new HashMap<>();//Creamos el map para la respuesta al cliente
@@ -39,5 +46,18 @@ public class EquipoController {
     public List<EquipoModel> mostrar(){
         //Invocamos el metodo creado en el servicio
         return this.equipoService.obtenerEquipos(); 
+    }
+
+    //Método para el manejo de errores
+    public void throwError(Errors error){
+        String mensaje="";
+        int index=0;
+        for(ObjectError e: error.getAllErrors()){
+            if(index>0){
+                mensaje +=" | ";
+            }
+            mensaje+=String.format("Parametro: %s - Mensaje: %s", e.getObjectName(),e.getDefaultMessage());
+        }
+        throw new CustomException(mensaje);
     }
 }
