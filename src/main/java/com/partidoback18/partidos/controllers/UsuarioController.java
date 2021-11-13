de881096eb170ef227ced7397991570bc2471ac4
Combatt17 <carloscombatt17@gmail.com>
2021-11-12 19:02:16 -0500

    agragando usuario y class BCrypt

---
+++ src/main/java/com/partidoback18/partidos/controllers/UsuarioController.java
@@ -0,0 +1,43 @@
+package com.partidoback18.partidos.controllers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.validation.Valid;
+
+import com.partidoback18.partidos.models.UsuarioModel;
+import com.partidoback18.partidos.services.UsuarioService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class UsuarioController {
+    
+    @Autowired
+    UsuarioService usuarioService;
+
+    //Definimos el método para agregar un usuario
+    @PostMapping("/usuarios")
+    public ResponseEntity<Map<String, String>> guardar(@Valid @RequestBody UsuarioModel usuario){
+        Map<String, String> respuesta= new HashMap<>();
+
+        UsuarioModel u=this.usuarioService.buscarUsername(usuario.getUsername());
+
+        if(u.getId()==null){
+            this.usuarioService.guardarUsuario(usuario);
+            respuesta.put("mensaje","Se agregó correctamente el usuario");
+
+        }else{
+            respuesta.put("mensaje","El usuario ya esta registrado");
+        }
+
+        return ResponseEntity.ok(respuesta);
+    }
+    
+}
---
+++ src/main/java/com/partidoback18/partidos/repositories/UsuarioRepository.java
@@ -0,0 +1,16 @@
+package com.partidoback18.partidos.repositories;
+
+import java.util.Optional;
+
+import com.partidoback18.partidos.models.UsuarioModel;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends MongoRepository<UsuarioModel,String>{
+    
+    //Para buscar a un usuario por nombre de usuario (username)
+    public Optional<UsuarioModel> findByUsername(String username);
+
+}
