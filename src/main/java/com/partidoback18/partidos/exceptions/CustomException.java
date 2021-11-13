cf5a5eee8055e3c3caca50c64692523d21362968
Combatt17 <carloscombatt17@gmail.com>
2021-11-13 18:22:11 -0500

    Añadiendo clases para el manejo de exceptions

---
+++ src/main/java/com/partidoback18/partidos/exceptions/CustomException.java
@@ -0,0 +1,9 @@
+package com.partidoback18.partidos.exceptions;
+
+public class CustomException extends RuntimeException {
+    
+    public CustomException(String msg){
+        super(msg);
+    }
+    
+}
